a0b2cb8bb4b072d0d0fce3b9382a77da8e063b6f
24pst03-del <24pst03@kpriet.ac.in>
2026-03-09 14:50:36 +0530

    Create 08_random_forest.py

---
+++ src/src/src/src/src/src/src/src/08_random_forest.py
@@ -0,0 +1,15 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+df = pd.read_csv("dataset/ggbs_sap_concrete_dataset_500_github_ready.csv")
+
+X = df.drop("Compressive_Strength_MPa", axis=1)
+y = df["Compressive_Strength_MPa"]
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+
+model = RandomForestRegressor()
+model.fit(X_train,y_train)
+
+print("Random Forest Model Ready")
